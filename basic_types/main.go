8418b388d8bba26282c88de7872a88ff1ac084fe
uzama <uzama@pickme.lk>
2021-05-23 12:39:35 +0530

    practiced with types

---
+++ basic_types/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var a int
+
+var (
+	b = 2
+	c = 3.45
+)
+
+const (
+	q = 1
+	w = 2 * 234
+	z = w < 44
+
+	l uint8 = 0x09
+	g uint8 = l & 0x7
+)
+
+func main() {
+	d := 6 // only inside function
+	e := 6.4
+
+	fmt.Println(a, b, c, d)
+	fmt.Printf("%4d: %7[1]T %[1]v\n", d)
+	fmt.Printf("%.2f: %7[1]T %[1]v\n", e)
+	fmt.Printf("%.2f: %7[1]T %[1]v\n", c)
+	d = int(c)
+	fmt.Printf("%4d: %7[1]T %[1]v\n", d)
+	e = float64(d)
+	fmt.Printf("%.2f: %7[1]T %[1]v\n", e)
+
+	fmt.Println("\n-------------")
+
+	var sum float64
+	var n int
+
+	for {
+		var val float64
+
+		if _, err := fmt.Fscanln(os.Stdin, &val); err != nil {
+			panic(err)
+		}
+
+		sum += val
+		n++
+
+		fmt.Println(sum / float64(n))
+	}
+}
